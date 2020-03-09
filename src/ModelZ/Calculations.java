b8666446e005d1de089fcd071f6bb0e7cdf335d0
alexs <alexs@192.168.0.227>
2020-03-09 11:28:59 +0100

    initial

---
+++ src/ModelZ/Calculations.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package ModelZ;
+
+/**
+ * Calculation methods for the SpaceZ Project
+ * @author Alexander Hristov
+ * @version 2020-03-09
+ */
+public class Calculations {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+	public double terminalVelocity(double m, double a, double p, double A, double Cd) {
+		return 0;
+	}
+}
